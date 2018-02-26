724ec62373d9b770c41511f5a029ee16300254df
cupururum <v.romashchenko@gmail.com>
2018-02-26 13:41:27 -0800

    start

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('California Giant Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hangover Burger');
+INSERT INTO burgers (burger_name) VALUES ('Meat Lovers Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon Burger');
